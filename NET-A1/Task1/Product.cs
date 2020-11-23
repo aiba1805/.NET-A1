2ab855cf095b83edb99be782129c32da931b8c9f
Aibek Shulembekov <aiba1805@mail.ru>
2020-11-23 17:08:37 +0600

    Debugging homework done

---
+++ NET-A1/Task1/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var product = obj as Product;
+            if (product.Name == this.Name &&
+                product.Price == this.Price)
+                return true;
+            return false;
+        }
+    }
+}
