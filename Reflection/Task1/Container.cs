11cf824f610eb7c0b052ef86f3501e4ebc1d2ded
Aibek_Shulembekov <Aibek_Shulembekov@EPKZASTW0418>
2021-01-13 11:42:41 +0600

    Reflection done

---
+++ Reflection/Task1/Container.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Task1.DoNotChange;
+
+namespace Task1
+{
+    public class Container
+    {
+
+        private List<Type> registeredTypes = new List<Type>();
+        private List<(Type, Type)> registeredBaseTypes = new List<(Type, Type)>();
+
+        public void AddAssembly(Assembly assembly)
+        {
+            foreach (var type in assembly.DefinedTypes)
+            {
+                if (type.CustomAttributes.Any(x => x.AttributeType == typeof(ImportConstructorAttribute)))
+                    AddType(type);
+                if (type.CustomAttributes.Any(x => x.AttributeType == typeof(ExportAttribute))){
+                    var attr = (ExportAttribute)Attribute.GetCustomAttribute(type, typeof(ExportAttribute));
+                    if (attr.Contract != null)
+                        AddType(type, attr.Contract);
+                    else
+                        AddType(type);
+                }
+                var hasImportProperties = type.GetProperties()
+                .Any(x => x.CustomAttributes
+                .Any(y => y.AttributeType == typeof(ImportAttribute)));
+                if(hasImportProperties)
+                    AddType(type);
+            };
+        }
+
+        public void AddType(Type type)
+        {
+            registeredTypes.Add(type);
+        }
+
+        public void AddType(Type type, Type baseType)
+        {
+            registeredBaseTypes.Add((type,baseType));
+        }
+
+        public T Get<T>()
+        {
+            if (!registeredTypes.Exists(x => x == typeof(T))
+                && !registeredBaseTypes.Any(x=>x.Item2 == typeof(T))) throw new Exception();
+            var type = typeof(T);
+            if (registeredBaseTypes.Any(x => x.Item2 == typeof(T))) 
+                type = registeredBaseTypes.Find(x => x.Item2 == typeof(T)).Item1;
+
+            var importConstructor = type.CustomAttributes.Any(x=>x.AttributeType == typeof(ImportConstructorAttribute));
+            if (importConstructor)
+            {
+                var constructor = type.GetConstructors().FirstOrDefault(x => x.GetParameters().Length > 0);
+                var parameters = new List<object>();
+                foreach (var parameter in constructor.GetParameters())
+                {
+                    var t = parameter.ParameterType;
+                    parameters.Add(this.GetType().GetMethod("Get")
+                        .MakeGenericMethod(t).Invoke(this, null));
+                }
+                return (T)constructor.Invoke(parameters.ToArray());
+            }
+
+            var importProperties = type.GetProperties()
+                .Where(x=> x.CustomAttributes
+                .Any(y => y.AttributeType == typeof(ImportAttribute)));
+            var typeValue = (T)Activator.CreateInstance(type);
+            foreach (var property in importProperties)
+            {
+                property.SetValue(typeValue, this.GetType().GetMethod("Get")
+                    .MakeGenericMethod(property.PropertyType).Invoke(this, null));
+            }
+            return typeValue;
+        }
+    }
+}
---
+++ Reflection/Task2/ObjectExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+
+namespace Task2
+{
+    public static class ObjectExtensions
+    {
+
+		public static void SetReadOnlyProperty(this object obj, string propertyName, object newValue)
+        {
+            var type = obj.GetType();
+            var property = obj.GetType().GetProperty(propertyName);
+            var backingField = type
+              .GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+              .FirstOrDefault(field =>
+                field.Attributes.HasFlag(FieldAttributes.Private) &&
+                field.Attributes.HasFlag(FieldAttributes.InitOnly) &&
+                field.CustomAttributes.Any(attr => attr.AttributeType == typeof(CompilerGeneratedAttribute)) &&
+                (field.DeclaringType == property.DeclaringType) &&
+                field.FieldType.IsAssignableFrom(property.PropertyType) &&
+                field.Name.StartsWith("<" + property.Name + ">"));
+            if(backingField == null)
+            {
+                type = type.BaseType ?? null;
+                backingField = type
+              .GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+              .FirstOrDefault(field =>
+                field.Attributes.HasFlag(FieldAttributes.Private) &&
+                field.Attributes.HasFlag(FieldAttributes.InitOnly) &&
+                field.CustomAttributes.Any(attr => attr.AttributeType == typeof(CompilerGeneratedAttribute)) &&
+                (field.DeclaringType == property.DeclaringType) &&
+                field.FieldType.IsAssignableFrom(property.PropertyType) &&
+                field.Name.StartsWith("<" + property.Name + ">"));
+            }
+            backingField.SetValue(obj, newValue);
+        }
+
+        public static void SetReadOnlyField(this object obj, string fieldName, object newValue)
+        {
+            var field = obj.GetType().GetField(fieldName);
+            var method = new DynamicMethod(
+            name: "SetField",
+            returnType: null,
+            parameterTypes: new[] { obj.GetType(), typeof(string) },
+            restrictedSkipVisibility: true
+        );
+            var gen = method.GetILGenerator();
+            gen.Emit(OpCodes.Ldarg_0);
+            gen.Emit(OpCodes.Ldarg_1);
+            gen.Emit(OpCodes.Stfld, field);
+            gen.Emit(OpCodes.Ret);
+            method.Invoke(null, new object[] { obj, newValue });
+        }
+    }
+}
