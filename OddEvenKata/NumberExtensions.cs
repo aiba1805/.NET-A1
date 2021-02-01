433c505288f1b3d41e40bdd512e623299ac893b3
Aibek_Shulembekov <Aibek_Shulembekov@EPKZASTW0418>
2021-02-01 13:36:13 +0600

    Finish Unit Tests module

---
+++ OddEvenKata/NumberExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OddEvenKata
+{
+    public static class NumberExtensions
+    {
+		public static bool IsPrime(this int number)
+		{
+			if (number <= 1) return false;
+			if (number == 2) return true;
+			if (number % 2 == 0) return false;
+
+			var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+			for (int i = 3; i <= boundary; i += 2)
+				if (number % i == 0)
+					return false;
+
+			return true;
+		}
+	}
+}
---
+++ OddEvenKata/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OddEvenKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var determinationList = new DeterminationList(1, 100);
+            var i = 1;
+            foreach (var item in determinationList)
+            {
+                Console.WriteLine($"{i++} - {item} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
