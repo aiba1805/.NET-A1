49f9cfaa50760658e4afa79ed0fc924a717a378f
Aibek_Shulembekov <Aibek_Shulembekov@EPKZASTW0418>
2021-02-01 13:37:21 +0600

    Move unit tests module to folder

---
+++ UnitTests/CalcStats/ArrayExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcStats
+{
+    public static class ArrayExtensions
+    {
+        public static int Min(this int[] arr)
+        {
+            var min = arr[0];
+            foreach (var item in arr)
+            {
+                if (min > item) min = item;
+            }
+            return min;
+        }
+
+        public static int Max(this int[] arr)
+        {
+            var max = arr[0];
+            foreach (var item in arr)
+            {
+                if (max < item) max = item;
+            }
+            return max;
+        }
+
+        public static int Avg(this int[] arr)
+        {
+            var sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+    
+            return sum / arr.Length;
+        }
+
+        public static int Count(this int[] arr)
+        {
+            var i = 0;
+            foreach (var item in arr)
+            {
+                i++;
+            }
+
+            return i;
+        }
+    }
+}
---
+++ UnitTests/LeapYear/LeapYearExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeapYear
+{
+    public static class LeapYearExtension
+    {
+        public static bool IsLeapYear(this DateTime datetime){
+            var year = datetime.Year;
+            if ((year % 4 == 0 && year % 100 != 0) 
+                || (year % 4 == 0 && year % 100 == 0 && year % 400 == 0))
+                return true;
+            else return false;
+        }
+    }
+}
